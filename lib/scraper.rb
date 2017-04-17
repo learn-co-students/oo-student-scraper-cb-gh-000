d782aedc2e796a7ab29aa4feef86d4841690c3d5
robertjosephwayne <robertjwayne@gmail.com>
2017-04-16 21:55:37 -0400

    Create Scraper.scrape_index_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,30 +3,29 @@ require 'pry'
 
 class Scraper
 
-=begin
-  Example return value:
-
-    [
-      {:name => "Abby Smith", :location => "Brooklyn, NY", :profile_url => "./fixtures/student-site/students/abby-smith.html"},
-      {:name => "Joe Jones", :location => "Paris, France", :profile_url => "./fixtures/student-site/students/joe-jonas.html"},
-      {:name => "Carlos Rodriguez", :location => "New York, NY", :profile_url => "./fixtures/student-site/students/carlos-rodriguez.html"},
-      {:name => "Lorenzo Oro", :location => "Los Angeles, CA", :profile_url => "./fixtures/student-site/students/lorenzo-oro.html"},
-      {:name => "Marisa Royer", :location => "Tampa, FL", :profile_url => "./fixtures/student-site/students/marisa-royer.html"}
-    ]
-=end
   def self.scrape_index_page(index_url)
-    # Initialize empty array to hold unique hashes for each student
+    students = []
 
-    # Get list of students
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
 
-    # For each student in the list, create a hash
-    # In each student hash, store the following attributes:
-    # Student name (e.g. "Abby Smith")
-    # Student location (e.g. "Brooklyn, NY")
-    # Student's profile URL (e.g. "./fixtures/student-site/students/abby-smith.html")
-    # Add student hash to the array of student hashes
+    # For each student in the list, create a hash with student's name, location, and profile URL
+    doc.css(".student-card").each do |student_card|
+      # Student name (e.g. "Abby Smith")
+      name = student_card.css(".student-name").text
+
+      # Student location (e.g. "Brooklyn, NY")
+      location = student_card.css(".student-location").text
+
+      # Student's profile URL (e.g. "./fixtures/student-site/students/abby-smith.html")
+      profile_url = "./fixtures/student-site/#{student_card.css("a").first["href"]}"
+
+      # Add student hash to the array of student hashes
+      students << { :name => name, :location => location, :profile_url => profile_url }
+    end
 
     # Return array of student hashes
+    students
   end
 
 =begin
@@ -42,27 +41,23 @@ class Scraper
      }
 =end
   def self.scrape_profile_page(profile_url)
-    # Create hash to hold attributes and values for the student
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
 
     # Scrape profile page for twitter link
-      # Add twitter link as the value for the :twitter attribute
 
     # Scrape profile page for linkedin link
-      # Add linkedin link as the value for the :linkedin attribute
 
     # Scrape profile page for github link
-      # Add github link as the value for the :github attribute
 
     # Scrape profile page for blog link
-      # Add blog link as the value for the :blog attribute
 
     # Scrape profile page for profile quote
-      # Add profile quote as the value for the :profile_quote attribute
 
     # Scrape profile page for bio
       # Add bio as the value for the :bio attribute
 
-    # Return hash of attributes
+    { :twitter => , :linkedin =>, :github => , :blog => , :profile_quote => , :bio =>  }
   end
 
 end
