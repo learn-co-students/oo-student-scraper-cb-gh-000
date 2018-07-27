5acc9e21fa8e587c7bbf8be686c769716b6bd52e
Quinn Dizon <qdizon@gmail.com>
2018-07-27 17:08:56 +0000

    work on scraper functionality

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,50 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    doc.css(".student-card a").each do |student|
+    name = student.css(".student-name").text
+    location = student.css(".student-location").text
+    profile_url = student.attr("href")
+    students << { :name => name, :location => location, :profile_url => profile_url }
+  end
+  students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
+    profile_data = {}
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+
+    doc.css(".social-icon-container a").each do |icon|
+        social = icon.attr("href")
+
+        case true
+          when social.include?("facebook")
+            profile_url[:facebook] = social
+          when social.include?("twitter")
+            profile_url[:twitter] = social
+          when social.include?("github")
+            profile_url[:github] = social
+          when social.include?("linkedin")
+            profile_url[:linkedin] = social
+          else
+            profile_url[:blog] = social
+        end
+        puts profile_url
+
+    end
+
+    puts linkedin
   end
 
 end
 
+Scraper.scrape_profile_page("./fixtures/student-site/students/eric-chu.html")
