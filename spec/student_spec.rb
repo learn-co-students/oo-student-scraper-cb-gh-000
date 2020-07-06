05dc452f632cdb86e4a6c58af9870d82ceed0ff0
Sarmad Riaz <ms30237@gmail.com>
2020-07-06 03:30:18 +0000

    Done.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,63 +1,75 @@
-require "spec_helper"
-
-describe "Student" do 
-  let!(:student_index_array) {[{:name=>"Alex Patriquin", :location=>"New York, NY"},
- {:name=>"Bacon McRib", :location=>"Kansas City, MO"},
- {:name=>"Alisha McWilliams", :location=>"San Francisco, CA"},
- {:name=>"Daniel Fenjves", :location=>"Austin, TX"},
- {:name=>"Arielle Sullivan", :location=>"Chicago, IL"},
- {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"},
- {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"}]}
-
- let!(:student_hash) {{:twitter=>"someone@twitter.com",
- :linkedin=>"someone@linkedin.com",
- :github=>"someone@github.com",
- :blog=>"someone@blog.com",
- :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
- :bio=>
-  "I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school."}}
-
-
-  let!(:student) {Student.new({:name=>"Alex Patriquin", :location=>"New York, NY"})}
-  
-  after(:each) do 
-    Student.class_variable_set(:@@all, [])
-  end
-  describe "#new" do
-    it "takes in an argument of a hash and sets that new student's attributes using the key/value pairs of that hash." do 
-      expect{Student.new({:name => "Sophie DeBenedetto", :location => "Brooklyn, NY"})}.to_not raise_error
-      expect(student.name).to eq("Alex Patriquin")
-      expect(student.location).to eq("New York, NY")
-    end 
-
-    it "adds that new student to the Student class' collection of all existing students, stored in the `@@all` class variable." do 
-      expect(Student.class_variable_get(:@@all).first.name).to eq("Alex Patriquin")
-    end
+require_relative "../lib/scraper.rb"
+require_relative "../lib/student.rb"
+require_relative "../config.rb"
+RSpec.configure do |config|
+  # rspec-expectations config goes here. You can use an alternate
+ 
+  # assertion/expectation library such as wrong or the stdlib/minitest
+  # assertions if you prefer.
+  config.expect_with :rspec do |expectations|
+    # This option will default to `true` in RSpec 4. It makes the `description`
+    # and `failure_message` of custom matchers include text for helper methods
+    # defined using `chain`, e.g.:
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger than 2 and smaller than 4"
+    # ...rather than:
+    #     # => "be bigger than 2"
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  describe ".create_from_collection" do 
-    it "uses the Scraper class to create new students with the correct name and location." do 
-      Student.class_variable_set(:@@all, [])
-      Student.create_from_collection(student_index_array)
-      expect(Student.class_variable_get(:@@all).first.name).to eq("Alex Patriquin")
-    end
+  # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
   end
 
-  describe "#add_student_attributes" do 
-    it "uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student." do 
-      student.add_student_attributes(student_hash) 
-      expect(student.bio).to eq("I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school.")
-      expect(student.blog).to eq("someone@blog.com")
-      expect(student.linkedin).to eq("someone@linkedin.com")
-      expect(student.profile_quote).to eq("\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi")
-      expect(student.twitter).to eq("someone@twitter.com")
-    end
+# The settings below are suggested to provide a good initial experience
+# with RSpec, but feel free to customize to your heart's content.
+=begin
+  # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  # Allows RSpec to persist some state between runs in order to support
+  # the `--only-failures` and `--next-failure` CLI options. We recommend
+  # you configure your source control system to ignore this file.
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  # Limits the available syntax to the non-monkey patched syntax that is
+  # recommended. For more details, see:
+  #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
+  #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+  #   - http://myronmars.to/n/dev-blog/2014/05/notable-changes-in-rspec-3#new__config_option_to_disable_rspeccore_monkey_patching
+  config.disable_monkey_patching!
+  # This setting enables warnings. It's recommended, but in some cases may
+  # be too noisy due to issues in dependencies.
+  config.warnings = true
+  # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
   end
-
-  describe '.all' do
-    it 'returns the class variable @@all' do
-      Student.class_variable_set(:@@all, [])
-      expect(Student.all).to match_array([])
-    end
-  end
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+=end
 end
